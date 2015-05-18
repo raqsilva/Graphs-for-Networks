feee359d9f2c2d8f22f8ae1e54c21728298aba25
raqsilva <raqsilva8@hotmail.com>
2015-05-18 13:39:49 +0100

    get kegg info

---
+++ get_kegg_info.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from bioservices import KEGG
+s = KEGG()
+s.organism = "hsa"
+x=s.pathwayIds
+
+res = s.get(x[0])
+
+modules=s.moduleIds
+
+mod=s.get(modules[0])
+dic=s.parse(mod)
+
