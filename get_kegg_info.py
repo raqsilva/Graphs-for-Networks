8666272ec17063c82b86220e1b81126bee0c9f06
raqsilva <raqsilva8@hotmail.com>
2015-05-18 13:50:03 +0100

    update

--- get_kegg_info.py
+++ get_kegg_info.py
@@ -3,12 +3,14 @@
 from bioservices import KEGG
 s = KEGG()
 s.organism = "hsa"
-x=s.pathwayIds
 
+x=s.pathwayIds
 res = s.get(x[0])
 
 modules=s.moduleIds
 
 mod=s.get(modules[0])
 dic=s.parse(mod)
-
+reactions=dic['REACTION']
+for reac in reactions:
+    print(reactions[reac])
