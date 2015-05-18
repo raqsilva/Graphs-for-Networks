a739e2ae38624646deca70b5ccc6f89604e82aa3
raqsilva <raqsilva8@hotmail.com>
2015-05-18 15:17:34 +0100

    updating, has key error Reaction
    key error "reaction", make try and except in cycle

--- get_kegg_info.py
+++ get_kegg_info.py
@@ -4,16 +4,19 @@ from bioservices import KEGG
 s = KEGG()
 s.organism = "hsa"
 
-x=s.pathwayIds
-res = s.get(x[0])
+#x=s.pathwayIds
+#res = s.get(x[0])
 
 modules=s.moduleIds
+dic_reac={}
+for ids in modules:
+    mod=s.get(ids)
+    dic=s.parse(mod)
+    reactions=dic['REACTION']
+    for reac in reactions:
+        teste=reactions[reac]
+        string=teste.split(" ")
+        dic_reac[reac]=string
 
-mod=s.get(modules[0])
-dic=s.parse(mod)
-reactions=dic['REACTION']
 #for reac in reactions:
 #    print(reactions[reac])
-
-teste=reactions["R01070"]
-string=teste.split(" ")
