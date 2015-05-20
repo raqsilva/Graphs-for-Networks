72d60ae1a03c7afb46ff079cbe0c6fd089eb84a0
raqsilva <raqsilva8@hotmail.com>
2015-05-20 14:21:23 +0100

    viewing dic

--- get_kegg_info.py
+++ get_kegg_info.py
@@ -8,15 +8,19 @@ s.organism = "hsa"
 #res = s.get(x[0])
 
 modules=s.moduleIds
-dic_reac={}
-for ids in modules:
-    mod=s.get(ids)
-    dic=s.parse(mod)
-    reactions=dic['REACTION']
-    for reac in reactions:
-        teste=reactions[reac]
-        string=teste.split(" ")
-        dic_reac[reac]=string
-
-#for reac in reactions:
-#    print(reactions[reac])
+#dic_reac={}
+# pick one organism or just the module (cycle) you want to create the network from
+# each module corresponds a cycle, a pathway
+#for ids in modules:
+#    mod=s.get(ids)
+#    dic=s.parse(mod)
+#    try:
+#        reactions=dic['REACTION']
+#        for reac in reactions:
+#            teste=reactions[reac]
+#            string=teste.split(" ")
+#            dic_reac[reac]=string
+#    except KeyError:
+#        pass
+        
+print(s.parse(s.get(modules[0])))
